d56c0c0baee85d73109799b61135918b306a90b7
Seth <sh.gonzalez311@gmail.com>
2024-03-15 19:20:10 -0600

    consulta del clima

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import AppClima from "./components/AppClima"
+import Formulario from "./components/Formulario"
+import { ClimaProvider } from  './context/ClimaProvider'
+
+
+function App() {
+
+  return (
+    <ClimaProvider>
+      <header>
+        <h1>Buscador de Clima</h1>
+      </header>
+      <AppClima/>
+    </ClimaProvider>
+  )
+}
+
+export default App
---
+++ src/components/AppClima.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Formulario from './Formulario'
+import Resultado from './Resultado'
+import Loading from './Loading'
+import useClima from '../hooks/useClima'
+
+
+const AppClima = () => {
+
+const {resultado, cargando, noResultado} = useClima();
+
+
+  return (
+    <>
+        <main className='dos-columnas'>
+            <Formulario/>
+
+            {   
+                cargando ? <Loading/> :
+                resultado?.name  ? <Resultado/>:
+                noResultado ? <p>{noResultado}</p>
+                : <p>Realiza una consulta</p>
+            }
+
+           
+        </main>
+    </>
+  )
+}
+
+export default AppClima
